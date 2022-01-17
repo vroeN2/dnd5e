227944e8e69281cf5199976031578c37dca9970a
Maciej Zachariasz <47660926+vroeN2@users.noreply.github.com>
2022-01-17 11:19:23 +0100

    feat: add assets and basic styling
    - add page background
    - add custom fontpacks
    - style navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,11 +2,15 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <h1>DnD 5.0 Spellist</h1>
-      <div className="links">
-        <Link to="/">Spell List</Link>
-        <Link to="/fav">Favourites</Link>
+    <nav className="navbar__wrapper">
+      <h1 className="page_title">DnD 5.0 Spellist</h1>
+      <div className="links__wrapper">
+        <Link to="/" className="navbar__link">
+          Spell List
+        </Link>
+        <Link to="/fav" className="navbar__link">
+          Favourites
+        </Link>
       </div>
     </nav>
   );
